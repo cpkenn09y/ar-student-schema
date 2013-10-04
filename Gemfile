73212dde41eb396143466b3e6fd71375138acc0d
danbender <mail@danbender.net>
2013-10-03 17:43:13 -0700

    join table between students and teachers

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'activerecord', :require => 'active_record'
+
+gem 'faker', '1.0.1'
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,12 +1,8 @@
 require_relative '../../db/config'
 
-class Student < ActiveRecord::Base
+class Teacher < ActiveRecord::Base
   validates :email, :uniqueness => true
   validates(:email, :format => {:with => /\w+@{1}\w+\.\w{2,}/, :message => 'nooooooo'})
 
-  has_and_belongs_to_many :teachers
-
-  def name
-    self[:first_name] + ' ' + self[:last_name]
-  end
+  has_and_belongs_to_many :students
 end
